0f4310b3a50afbaf3a5d21e378a15bb4cab9a46b
deflis <deflis@gmail.com>
2022-10-05 22:57:06 +0900

    メニューバーを実装

--- packages/ui/components/Button.tsx
+++
@@ -1,5 +0,0 @@
-import { button } from "./Button.module.css";
-
-export const Button: React.FC<{ children?: string }> = ({ children }) => {
-  return <button className={button}>{children}</button>;
-};
--- packages/ui/components/MenuBar.tsx
+++ packages/ui/components/MenuBar.tsx
@@ -1,7 +1,14 @@
-import { forwardRef } from "react";
+import { forwardRef, Fragment } from "react";
 import clsx from "clsx";
-import { menubar } from "./MenuBar.module.css";
-import { BsFileTextFill } from "react-icons/bs";
+import {
+  menubar,
+  menuitem,
+  items,
+  item,
+  item_active,
+} from "./MenuBar.module.css";
+import { BsCheck, BsFileTextFill } from "react-icons/bs";
+import { Menu, Transition } from "@headlessui/react";
 
 export const MenuBar = forwardRef<HTMLDivElement, { className?: string }>(
   ({ className }, ref) => {
@@ -10,10 +17,64 @@ export const MenuBar = forwardRef<HTMLDivElement, { className?: string }>(
         <span>
           <BsFileTextFill />
         </span>
-        <span>File</span>
-        <span>View</span>
+
+        <Menu as="div" className={menuitem}>
+          <Menu.Button>ファイル</Menu.Button>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className={items}>
+              <div>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button className={clsx(item, active && item_active)}>
+                      開く
+                    </button>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button className={clsx(item, active && item_active)}>
+                      保存する
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+        <Menu as="div" className={menuitem}>
+          <Menu.Button>表示</Menu.Button>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className={items}>
+              <div>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button className={clsx(item, active && item_active)}>
+                      <BsCheck /> プレビューモード
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
         <span className="flex-grow"></span>
-        <span></span>
+        <span>プレビューモード</span>
       </header>
     );
   }
